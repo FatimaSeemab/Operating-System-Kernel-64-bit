481d356d23a04f3f56a574a546ec0b4acc31dfce
FatimaSeemab <61909305+FatimaSeemab@users.noreply.github.com>
2021-04-20 20:37:46 +0500

    Add files via upload

--- main.c
+++ main.c
@@ -3,5 +3,44 @@
 void kernel_main() {
     print_clear();
     print_set_color(PRINT_COLOR_YELLOW, PRINT_COLOR_BLACK);
-    print_str("SEECS");
+    print_str("*******"); print_str("   "); 
+    print_str("*******");  print_str("   "); 
+    print_str("*******"); print_str("   ");  
+    print_str("*******"); print_str("   "); 
+    print_str("*******"); print_str("   "); 
+    print_char('\n');
+      for(int i=0;i<6;i++)
+  { print_str("*      "); print_str("   "); 
+    print_str("*      "); print_str("   "); 
+    print_str("*      "); print_str("   "); 
+    print_str("*      "); print_str("   "); 
+    print_str("*      "); print_str("   "); 
+    print_char('\n');}
+    print_str("*******"); print_str("   ");
+    print_str("*******"); print_str("   ");
+    print_str("*******"); print_str("   ");
+    print_str("       "); print_str("   "); 
+    print_str("*******"); print_str("   "); 
+    print_char('\n');
+     for(int i=0;i<6;i++)
+    {print_str("      "); print_str("*"); 
+    print_str("   "); print_str("*      ");
+    print_str("   "); print_str("*      ");
+    print_str("   "); print_str("*      ");
+    print_str("   "); print_str("      *");
+     print_char('\n');
+    }
+
+    print_str("*******"); 
+    print_str("   "); 
+    print_str("*******"); 
+     print_str("   "); 
+    print_str("*******"); 
+    print_str("   ");  
+    print_str("*******"); 
+    print_str("   "); 
+    print_str("*******"); print_str("   "); 
+    
+      print_char('\n');
+
 }
